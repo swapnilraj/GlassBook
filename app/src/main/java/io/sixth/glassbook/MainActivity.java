a10b67d29303f711dc40f3329c68acc034978b05
Swapnil Raj <raj.swapnil99@gmail.com>
2016-12-26 00:14:32 +0530

    Add base project

---
+++ app/src/main/java/io/sixth/glassbook/MainActivity.java
@@ -0,0 +1,17 @@
+package io.sixth.glassbook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by thawne on 25/12/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+  }
+}
